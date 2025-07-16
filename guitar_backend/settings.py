272908b2344798fd8d2f6ec0341b748616fd1c59
KWilanF <kentwilanfegarido@gmail.com>
2025-07-16 09:54:03 +0800

    Update settings.py

--- guitar_backend/settings.py
+++ guitar_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'guitar_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("django-db"),
+        'USER': os.getenv("django_db_e2qs_user"),
+        'PASSWORD': os.getenv("PyAon8mMpbvdBtuQgJvjyCJAucQQep2Y"),
+        'HOST': os.getenv("dpg-d1r18lodl3ps73eua030-a"),
+        'PORT': os.getenv("5432"),
     }
 }
 
