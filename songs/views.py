3d3ee5524b786b030fa39169d5641eba7f5010f6
KWilanF <kentwilanfegarido@gmail.com>
2025-07-16 18:05:44 +0800

    Update views.py

--- songs/views.py
+++ songs/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from .models import Song
 from .serializers import SongSerializer
 
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+    permission_classes = [IsAdminUser]  # Only admin users can access
